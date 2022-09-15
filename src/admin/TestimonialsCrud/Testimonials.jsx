8062db3ed34a4f92c4f685411b7e5f9c594dfeda
Edward Guerrero <iamwar2070@gmail.com>
2022-09-15 01:18:16 -0400

    feat: add reading testimonials function

--- src/admin/TestimonialsCrud/Testimonials.jsx
+++ src/admin/TestimonialsCrud/Testimonials.jsx
@@ -20,6 +20,9 @@ import AddIcon from "@material-ui/icons/Add";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import CardMedia from "@material-ui/core/CardMedia";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 
 function getStyles(type, theme) {
   return {
@@ -31,6 +34,11 @@ function getStyles(type, theme) {
 }
 
 const useStyles = makeStyles((theme) => ({
+  cardMedia: {
+    paddingTop: "81.25%",
+    borderRadius: "50%",
+    margin: "28px",
+  },
   loading: {
     display: "flex",
     "& > * + *": {
@@ -46,7 +54,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
     margin: "auto",
-    width: "100%",
+    marginBottom: "20px",
   },
   input: {
     padding: "2",
@@ -78,7 +86,7 @@ export default function Testimonials() {
   const [name, setName] = useState("");
   const [value, setValue] = useState("");
   const [footer, setFooter] = useState("");
-  // const [tiles, setTiles] = useState([]);
+  const [tiles, setTiles] = useState([]);
   const [backdrop, setBackdrop] = useState(true);
   const theme = useTheme();
   const [avatarObj, setAvatarObj] = useState("");
@@ -89,12 +97,12 @@ export default function Testimonials() {
   });
 
   const [loading, setLoading] = useState(false);
-  const [buttonState, setButtonState] = useState(true);
-  const history = useHistory();
+  // const [buttonState, setButtonState] = useState(true);
+  // const history = useHistory();
 
-  const setValueText = (event) => {
-    setValue(event.target.value);
-  };
+  // const setValueText = (event) => {
+  //   setValue(event.target.value);
+  // };
   const handleChangeType = (event) => {
     setType(event.target.value);
   };
@@ -102,15 +110,14 @@ export default function Testimonials() {
   const [errorMessage, setErrorMessage] = useState();
   const [snackBarError, setSnackBarError] = useState(false);
 
-  // useEffect(() => {
-  //   const base_url =
-  //     process.env.REACT_APP_BACKEND_URL + "/testimonial/read-all";
-
-  //   axios.get(base_url).then((response) => {
-  //     setTiles(utils.shuffle(response.data.Testimonials));
-  //     setBackdrop(false);
-  //   });
-  // }, []);
+  useEffect(() => {
+    const base_url =
+      process.env.REACT_APP_BACKEND_URL + "/testimonial/read-all";
+    axios.get(base_url).then((response) => {
+      setTiles(response.data.testimonials);
+      setBackdrop(false);
+    });
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -120,7 +127,7 @@ export default function Testimonials() {
       e.preventDefault();
     } else {
       setLoading(true);
-      setButtonState(true);
+      // setButtonState(true);
 
       const formData = new FormData();
       formData.append("avatar", avatarPic);
@@ -137,9 +144,6 @@ export default function Testimonials() {
         "Content-Type": "multipart/form-data",
       });
       setLoading(false);
-
-      // .then(console.log)
-      // .catch(console.log);
     }
   };
 
@@ -152,40 +156,44 @@ export default function Testimonials() {
       setInputChange(true);
       setAvatarObj(URL.createObjectURL(e.target.files[0]));
       setAvatarPic(e.target.files[0]);
-      console.log(avatar);
-      console.log(avatarPic);
     } //Avatar
   };
-
+  const deleteTestimonial = async (d) => {
+    d.preventDefault();
+    setLoading(true);
+    const base_url = process.env.REACT_APP_BACKEND_URL + "/testimonial/delete";
+    let res = await axios.delete(base_url);
+    setLoading(false);
+  };
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
-  const handleTestimonialDataEdit = async () => {
-    setBackdrop(true);
-    var formData = new FormData();
-    if (inputChange) {
-      formData.append("avatar", avatarPic);
-    }
-    formData.append("type", type);
-    formData.append("name", name);
-    formData.append("value", value);
-    formData.append("footer", footer);
-    formData.append("state", state);
+  // const handleTestimonialDataEdit = async () => {
+  //   setBackdrop(true);
+  //   var formData = new FormData();
+  //   if (inputChange) {
+  //     formData.append("avatar", avatarPic);
+  //   }
+  //   formData.append("type", type);
+  //   formData.append("name", name);
+  //   formData.append("value", value);
+  //   formData.append("footer", footer);
+  //   formData.append("state", state);
 
-    const base_url = process.env.REACT_APP_BACKEND_URL + "/testimonial/update";
-    const response = await axios.put(base_url, formData);
-    if (response.data) {
-      setAvatar(response.data.avatar);
-      setType(response.data.type);
-      setName(response.data.name);
-      setValue(response.data.value);
-      setFooter(response.data.footer);
-      setAvatarObj(response.data.avatar);
-      setAvatarPic(response.data.avatar);
-      setBackdrop(false);
-    } else {
-      setBackdrop(false);
-    }
-  };
+  //   const base_url = process.env.REACT_APP_BACKEND_URL + "/testimonial/update";
+  //   const response = await axios.put(base_url, formData);
+  //   if (response.data) {
+  //     setAvatar(response.data.avatar);
+  //     setType(response.data.type);
+  //     setName(response.data.name);
+  //     setValue(response.data.value);
+  //     setFooter(response.data.footer);
+  //     setAvatarObj(response.data.avatar);
+  //     setAvatarPic(response.data.avatar);
+  //     setBackdrop(false);
+  //   } else {
+  //     setBackdrop(false);
+  //   }
+  // };
   return (
     <div className={classes.root}>
       <Backdrop className={classes.backdrop} open={loading}>
@@ -226,9 +234,9 @@ export default function Testimonials() {
             item
             xs={12}
             sm={12}
-            md={8}
-            lg={8}
-            xl={8}
+            md={12}
+            lg={10}
+            xl={10}
           >
             <Paper
               style={{ padding: isMobile ? "8px" : "24px" }}
@@ -439,178 +447,97 @@ export default function Testimonials() {
             </Paper>
           </Grid>
 
-          <Grid
-            style={{ padding: isMobile ? "0px" : "24px" }}
-            // padding={"24px"}
-            className={classes.paper}
-            // display={"flex"}
-            item
-            xs={12}
-            sm={12}
-            md={6}
-            lg={6}
-            xl={6}
-          >
-            <Paper
-              style={{ padding: isMobile ? "4px" : "24px" }}
-              className={classes.paper}
-              xs={12}
-              sm={12}
-            >
-              <Grid container spacing={1} style={{ paddingBottom: "10px" }}>
-                {loading && (
-                  <div className={classes.loading}>
-                    <CircularProgress />
-                  </div>
-                )}
-                <Box marginBottom={2}>
-                  {avatarObj ? (
-                    <Avatar className={classes.avatar}>
-                      <label htmlFor="file-input">
-                        <img
-                          src={avatarObj}
-                          alt="Prixer profile avatar"
-                          style={{ maxHeight: 65 }}
-                        />
-                      </label>
-                      <input
-                        style={{ display: "none" }}
-                        accept="image/*"
-                        id="file-input"
-                        type="file"
-                        onChange={onImageChange}
-                        required
-                      />
-                    </Avatar>
-                  ) : (
-                    <Avatar className={classes.avatar}>
-                      <label htmlFor="file-input">
-                        <AddIcon
-                          style={{
-                            width: 60,
-                            height: 60,
-                            color: "#d33f49",
-                          }}
-                        />
-                      </label>
-                      <input
-                        style={{ display: "none" }}
-                        accept="image/*"
-                        id="file-input"
-                        type="file"
-                        onChange={onImageChange}
-                      />
-                    </Avatar>
-                  )}
-                </Box>
-                <Grid
-                  item
-                  xs={6}
-                  padding={10}
-                  display={"flex"}
-                  alignItems={"center"}
-                >
-                  <InputLabel style={{ fontSize: ".85em" }}>Tipo</InputLabel>
-                  <Select
-                    style={{ width: "100%" }}
-                    labelId="tipo"
-                    id="tipo"
-                    value={type}
-                    onChange={(e) => handleChangeType(e)}
-                    MenuProps={MenuProps}
-                  >
-                    {["Prixer", "Compañía", "Cliente"].map((type) => (
-                      <MenuItem
-                        key={type}
-                        value={type}
-                        style={getStyles(type, theme)}
-                      >
-                        {type}
-                      </MenuItem>
-                    ))}
-                    {/* <option value="Prixer">Prixer</option>
-                      <option value="Compañía">Compañía</option>
-                      <option selected value="Cliente">
-                        Cliente
-                      </option> */}
-                  </Select>
-                </Grid>
-                <Grid item xs={6} sm={6}>
-                  <TextField
-                    autoComplete="fname"
-                    name="name"
-                    variant="outlined"
-                    fullWidth
-                    type="text"
-                    value={name}
-                    id="name"
-                    label="Nombre"
-                    autoFocus
-                    onChange={(e) => {
-                      setName(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12} sm={12}>
-                  <TextField
-                    autoComplete="fname"
-                    name="value"
-                    variant="outlined"
-                    fullWidth
-                    type="text"
-                    value={value}
-                    id="value"
-                    label="Body"
-                    autoFocus
-                    multiline
-                    rows={3}
-                    onChange={(e) => {
-                      setValue(e.target.value);
-                    }}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    variant="outlined"
-                    fullWidth
-                    name="footer"
-                    label="Footer"
-                    type="text"
-                    value={footer}
-                    inputProps={{ maxLenght: 300 }}
-                    id="footer"
-                    autoComplete="fname"
-                    multiname
-                    onChange={(e) => {
-                      setFooter(e.target.value);
-                    }}
-                  />
-                </Grid>
-
-                <Grid item xs={3}>
-                  <Switch
-                    checked={state /*.checkedA*/}
-                    onChange={handleChange}
-                    name="checkedA"
-                    value={state}
-                    inputProps={{ "aria-label": "secondary checkbox" }}
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                value="submit"
-                paddingTop="4"
-                onClick={handleSubmit}
+          {tiles &&
+            tiles.map((tile) => (
+              <Paper
+                key={tile._id}
+                style={{
+                  padding: isMobile ? "4px" : "24px",
+                }}
+                className={classes.paper}
+                item
+                // style={{ padding: isMobile ? "18px 0px" : "24px 24px" }}
+                xs={6}
+                sm={6}
+                md={4}
+                lg={4}
+                xl={4}
               >
-                Crear testimonio
-              </Button>
-            </Paper>
-          </Grid>
+                <Grid container spacing={1} style={{ padding: "10px" }}>
+                  {loading && (
+                    <div className={classes.loading}>
+                      <CircularProgress />
+                    </div>
+                  )}
+                  <Grid
+                    marginBottom={2}
+                    item
+                    xs={4}
+                    style={{ display: "flex", justifyContent: "center" }}
+                  >
+                    <Avatar className={classes.avatar}>
+                      <CardMedia
+                        alt={tile.title}
+                        height="60"
+                        image={tile.avatar}
+                        className={classes.cardMedia}
+                        title={tile.title}
+                      />
+                      <img
+                        src={avatarPic}
+                        alt="testimonial avatar"
+                        style={{ maxHeight: 200 }}
+                      />
+                    </Avatar>
+                  </Grid>
+                  <Grid
+                    item
+                    xs
+                    padding={10}
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Typography>{tile.name}</Typography>
+                    <Typography variant={"p"} color={"secondary"}>
+                      {tile.type}
+                    </Typography>
+                  </Grid>
+                  <Box item sm={8}>
+                    <EditIcon color={"secondary"} />{" "}
+                    <DeleteIcon
+                      color={"secondary"}
+                      onClick={deleteTestimonial}
+                    />
+                  </Box>
+                  <Grid item xs={12}>
+                    <Typography variant={"body2"}>{tile.value}</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant={"body2"} color="secondary">
+                      {tile.footer}
+                    </Typography>
+                  </Grid>
+
+                  <Grid
+                    item
+                    xs={12}
+                    style={{ display: "flex", justifyContent: "end" }}
+                  >
+                    <Switch
+                      checked={tile.state /*.checkedA*/}
+                      color="primary"
+                      onChange={handleChange}
+                      name="checkedA"
+                      value={tile.state}
+                      inputProps={{ "aria-label": "secondary checkbox" }}
+                    />
+                  </Grid>
+                </Grid>
+              </Paper>
+            ))}
         </Grid>
       </Paper>
     </div>
--- src/sharedComponents/prixerGrid/prixerGrid.jsx
+++ src/sharedComponents/prixerGrid/prixerGrid.jsx
@@ -1,51 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
 import { useHistory } from "react-router-dom";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import utils from '../../utils/utils';
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import utils from "../../utils/utils";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
     // maxWidth: "70%",
     backgroundColor: theme.palette.background.paper,
   },
   gridList: {
-    width: '100%',
-    height: 'auto'
+    width: "100%",
+    height: "auto",
   },
   img: {
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
   },
   icon: {
-    color: 'rgba(255, 255, 255, 0.54)',
+    color: "rgba(255, 255, 255, 0.54)",
   },
   cardGrid: {
     paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(8)
+    paddingBottom: theme.spacing(8),
   },
   card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   cardMedia: {
-    paddingTop: '81.25%',
-    borderRadius: '50%',
-    margin: '28px'
+    paddingTop: "81.25%",
+    borderRadius: "50%",
+    margin: "28px",
   },
   cardContent: {
     flexGrow: 1,
@@ -56,57 +56,68 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
 export default function PrixerGrid(props) {
   const classes = useStyles();
   const [tiles, setTiles] = useState([]);
   const history = useHistory();
   const [backdrop, setBackdrop] = useState(true);
 
-  useEffect(()=> {
-    const base_url= process.env.REACT_APP_BACKEND_URL + "/prixer/read-all-full";
-  
-    axios.get(base_url)
-      .then(response =>{
-        setTiles(utils.shuffle(response.data.prixers));
-        setBackdrop(false);
+  useEffect(() => {
+    const base_url =
+      process.env.REACT_APP_BACKEND_URL + "/prixer/read-all-full";
+
+    axios.get(base_url).then((response) => {
+      setTiles(utils.shuffle(response.data.prixers));
+      setBackdrop(false);
     });
-}, []);
+  }, []);
 
   return (
     <div className={classes.root}>
-    <Backdrop className={classes.backdrop} open={backdrop}>
-      <CircularProgress color="inherit" />
-    </Backdrop>
-    <Grid container spacing={1}>
-    {tiles &&
-    tiles.filter(tile => tile.avatar).map((tile) =>
-      (
-        <Grid item key={tile._id} xs={6} sm={6} md={3}>
-            <Card className={classes.card}>
-            <CardMedia
-                alt={tile.title}
-                height= '100'
-                image={tile.avatar}
-                className={classes.cardMedia}
-                title={tile.title}
-            />
-            <CardContent className={classes.cardContent}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {tile.firstName} {tile.lastName}
-                </Typography>
-                <Typography gutterBottom variant="h6" component="h6" style={{fontSize: 16}}>
-                  {tile.username} - {tile.specialty}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small" color="primary" onClick={e => history.push({pathname:"/"+tile.username})}>
-                Explorar
-                </Button>
-            </CardActions>
-            </Card>
-        </Grid>
-    ))}
+      <Backdrop className={classes.backdrop} open={backdrop}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <Grid container spacing={1}>
+        {tiles &&
+          tiles
+            .filter((tile) => tile.avatar)
+            .map((tile) => (
+              <Grid item key={tile._id} xs={6} sm={6} md={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    alt={tile.title}
+                    height="100"
+                    image={tile.avatar}
+                    className={classes.cardMedia}
+                    title={tile.title}
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {tile.firstName} {tile.lastName}
+                    </Typography>
+                    <Typography
+                      gutterBottom
+                      variant="h6"
+                      component="h6"
+                      style={{ fontSize: 16 }}
+                    >
+                      {tile.username} - {tile.specialty}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={(e) =>
+                        history.push({ pathname: "/" + tile.username })
+                      }
+                    >
+                      Explorar
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
       </Grid>
     </div>
   );

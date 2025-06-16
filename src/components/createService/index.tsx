04135b2d26f0e02fe743d13564555ae3578d6be9
megalinker <digimastermegalinker@gmail.com>
2025-06-16 16:04:59 -0400

    bugfix: type update for prixerService

--- src/components/createService/index.tsx
+++ src/components/createService/index.tsx
@@ -432,7 +432,7 @@ export default function CreateService() {
       location: location,
       productionTime: productionTime,
       publicPrice: { from: Number(priceFrom), to: Number(priceTo) },
-      prixer: user ? user._id?.toString() : undefined,
+      userId: user ? user._id?.toString() : undefined,
       sources: { images: serviceImages },
       active: active,
     }
